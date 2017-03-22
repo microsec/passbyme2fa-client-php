467defc73c58c2db08d500fa7c4f011a8ec5d8de
Microsec <develop@microsec.hu>
2017-03-22 16:53:28 +0100

    hotfix

--- PassByME/Autoloader.php
+++ PassByME/Autoloader.php
@@ -5,7 +5,7 @@ namespace PassByME;
 /**
  * Autoloader functionality for loading classes.
  * Use this if you do not use composer autoload.
- * 
+ *
  * @author     Microsec Ltd. <development@passbyme.com>
  * @copyright  (c) 2015, Microsec Ltd.
  */
@@ -15,17 +15,15 @@ namespace PassByME;
  * Load files by namespace convention.
  *
  * @param string $namespace
- * @throws \Exception
  */
 function load($namespace)
 {
+
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
     $classFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . $path . '.php';
-
     if (!is_file($classFile)) {
-        throw new \Exception('Autoloader error! No such file: ' . $classFile);
+        trigger_error('Autoloader error! Cannot load file: ' . $classFile, E_USER_ERROR);
     }
-
     /** @noinspection PhpIncludeInspection */
     include_once($classFile);
 }
